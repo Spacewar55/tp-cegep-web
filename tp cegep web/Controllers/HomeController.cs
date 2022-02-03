a5fbf5663cf257aebec5a9ac8b9647248859e9f6
Alex De Souza <samgratte@chevet.com>
2022-02-03 13:54:02 -0500

    Ajoutez des fichiers projet.

---
+++ tp cegep web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tp_cegep_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
