b7122e56e1548843c8d18534e357637713901b20
Alex De Souza <samgratte@chevet.com>
2022-02-08 08:12:38 -0500

    ajout de la liste des départements

---
+++ tp cegep web/Controllers/DepartementsController.cs
@@ -0,0 +1,30 @@
+﻿using GestionCegepWeb.Logics.Controleurs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace tp_cegep_web.Controllers
+{
+    public class DepartementController : Controller
+    {
+        [Route("Departement")]
+        [Route("Departement/Index")]
+        [HttpGet]
+        public IActionResult Index([FromQuery] string nomCegep)
+        {
+            try
+            {
+                //nomCegep = "Cégep de Rivière-du-Loup";
+                nomCegep = "Cégep de la Pocatière";
+                //Préparation des données pour la vue...
+                ViewBag.ListeDepartement = CegepControleur.Instance.ObtenirListeDepartement(nomCegep).ToArray();
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            //Retour de la vue...
+            return View();
+        }
+    }
+}
+
